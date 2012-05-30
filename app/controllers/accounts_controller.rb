3181e92162c9e7f378e5eb0e793b763679f7ef38
Eddie A Tejeda <eddie@codeforamerica.org>
2012-05-30 01:59:13 -0500

    add basic backend interface for subscription

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,6 +2,8 @@ class AccountsController < ApplicationController
   before_filter :authenticate_account!
 
   def index
+    street_name = AddressHelpers.get_street_name('Van Ave')
+    @my_addresses = Address.find_addresses_with_cases_by_street(street_name).page(params[:page]).order(:house_num)
   end
   
   def show
