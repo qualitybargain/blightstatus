cde0cbaf3962e10bb7a21a281b3252fbb534b03f
Eddie A Tejeda <eddie@codeforamerica.org>
2012-05-31 14:58:40 -0500

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+admin = Account.create! do |u|
+  u.email = 'neworleans@codeforamerica.org'  
+  u.password = 'password'  
+  u.password_confirmation = 'password'  
+end
