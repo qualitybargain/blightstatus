16da3c8cbd62c86649d0f56558873e52b43fb131
Eddie A Tejeda <eddie@codeforamerica.org>
2012-06-01 17:45:38 -0500

    fix duplicate street names in autocomplate

--- app/controllers/streets_controller.rb
+++ app/controllers/streets_controller.rb
@@ -3,4 +3,16 @@ class StreetsController < ApplicationController
   respond_to :html, :json, :xml	
   autocomplete :street, :full_name
 
+  def autocomplete_street_full_name
+    term = params[:term]
+    if term && !term.empty?
+        items = Street.select("DISTINCT full_name").
+            where("LOWER(full_name) LIKE ?", term.downcase + '%').
+            limit(10).order(:full_name)
+     else
+       items = {}
+     end
+     render :json => json_for_autocomplete(items, :full_name)
+  end
+  
 end
