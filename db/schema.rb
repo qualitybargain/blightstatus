2b4a7249c7d79be46c26313c79df94b6e2a7c2b3
Serena Wales <serenawr@gmail.com>
2012-08-29 16:05:27 -0700

    Updates to lama import script

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120828073708) do
+ActiveRecord::Schema.define(:version => 20120829212803) do
 
   create_table "accounts", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -43,9 +43,9 @@ ActiveRecord::Schema.define(:version => 20120828073708) do
     t.float    "x"
     t.float    "y"
     t.string   "status"
-    t.datetime "created_at",                                                 :null => false
-    t.datetime "updated_at",                                                 :null => false
-    t.spatial  "point",            :limit => {:srid=>-1, :type=>"geometry"}
+    t.datetime "created_at",                                                :null => false
+    t.datetime "updated_at",                                                :null => false
+    t.spatial  "point",            :limit => {:srid=>0, :type=>"geometry"}
     t.string   "parcel_id"
     t.boolean  "official"
     t.string   "street_full_name"
@@ -132,6 +132,7 @@ ActiveRecord::Schema.define(:version => 20120828073708) do
     t.string   "case_number"
     t.datetime "created_at",         :null => false
     t.datetime "updated_at",         :null => false
+    t.string   "hearing_type"
   end
 
   add_index "hearings", ["case_number"], :name => "index_hearings_on_case_number"
@@ -145,6 +146,7 @@ ActiveRecord::Schema.define(:version => 20120828073708) do
     t.integer  "inspector_id"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.text     "notes"
   end
 
   add_index "inspections", ["case_number"], :name => "index_inspections_on_case_number"
@@ -190,9 +192,9 @@ ActiveRecord::Schema.define(:version => 20120828073708) do
     t.float    "x_max"
     t.float    "y_max"
     t.float    "area"
-    t.datetime "created_at",                                           :null => false
-    t.datetime "updated_at",                                           :null => false
-    t.spatial  "the_geom",   :limit => {:srid=>-1, :type=>"geometry"}
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.text     "the_geom"
   end
 
   create_table "notifications", :force => true do |t|
@@ -242,9 +244,9 @@ ActiveRecord::Schema.define(:version => 20120828073708) do
     t.string   "full_name"
     t.integer  "length_numberic"
     t.integer  "shape_len"
-    t.datetime "created_at",                                                 :null => false
-    t.datetime "updated_at",                                                 :null => false
-    t.spatial  "the_geom",         :limit => {:srid=>-1, :type=>"geometry"}
+    t.datetime "created_at",                                                :null => false
+    t.datetime "updated_at",                                                :null => false
+    t.spatial  "the_geom",         :limit => {:srid=>0, :type=>"geometry"}
     t.string   "prefix_direction"
     t.string   "suffix_direction"
   end
@@ -253,9 +255,9 @@ ActiveRecord::Schema.define(:version => 20120828073708) do
     t.integer  "address_id"
     t.integer  "account_id"
     t.string   "notes"
-    t.datetime "created_at",                                           :null => false
-    t.datetime "updated_at",                                           :null => false
-    t.spatial  "thegeom",    :limit => {:srid=>-1, :type=>"geometry"}
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.text     "thegeom"
   end
 
 end
