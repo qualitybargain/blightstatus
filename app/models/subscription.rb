b3ca57fdd1ab8a34b9982a2c6b572c64fb0a1450
Eddie A Tejeda <eddie@codeforamerica.org>
2012-08-30 23:40:15 -0700

    [WIP] implement send notifications method

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,22 @@
 class Subscription < ActiveRecord::Base
   belongs_to :account
   belongs_to :address
+
+  def self.send_notifications
+    accounts = Account.find(:all)
+
+	# TODO: need to make sure not to send duplicates
+    accounts.each{ | account | 
+      subscriptions = Subscription.find_all_by_account_id(account)
+
+      SubscriptionMailer.subscription_email(account, subscriptions).deliver
+
+      subscriptions.each{ | subscription |
+      	subscription.date_notified = Time.now
+      	subscription.save!
+      }
+    }
+
+  end
+
 end
