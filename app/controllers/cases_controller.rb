57213dbfcf85cb03c11a3615044eedd5a6931cba
Eddie A Tejeda <eddie@codeforamerica.org>
2012-09-02 13:07:38 -0700

    get Cases model to print json responses based on query for visualizations

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -1,9 +1,31 @@
 class CasesController < ApplicationController
-  respond_to :html, :json, :xml
+  respond_to :html, :json
 
   def index
-    @cases = Case.page(params[:page])
-    respond_with(@cases)
+
+    case params[:type]
+      when 'inspections'
+        @cases = Address.joins(:inspections).where(" inspection_date > '2012-01-01 11:00:00' AND inspection_date < '2012-01-29 11:00:00' ").pluck(:point)
+      when 'hearings'
+        @cases = Address.joins(:hearings).where(" hearing_date > '2012-01-01 11:00:00' AND hearing_date < '2012-01-29 11:00:00' ").pluck(:point)
+      when 'judgements'
+        @cases = Address.joins(:judgements).where(" judgement_date > '2012-01-01 11:00:00' AND judgement_date < '2012-01-29 11:00:00' ").pluck(:point)
+      when 'demolitions'
+        @cases = Address.joins(:demolitions).where(" date_completed > '2012-01-01 11:00:00' AND date_completed < '2012-01-29 11:00:00' ").pluck(:point)
+      else
+        @cases = Case.page(params[:page])
+    end
+
+
+    polygon = Subscription.last.thegeom
+    geojson = RGeo::GeoJSON::encode(polygon)
+
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render :json => @cases.to_json }
+    end
+      
   end
 
   def show
--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,10 +1,14 @@
 class StatisticsController < ApplicationController
 
-  respond_to :html, :json, :xml
+  respond_to :html, :json
 
   def index
   end
 
   def graphs
   end
+
+  def maps
+  end
+
 end
