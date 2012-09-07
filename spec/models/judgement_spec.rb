0f1924eca7e99658668175a312c45f108e42091f
Amir Reavis-Bey <amirbey@gmail.com>
2012-09-07 14:11:47 -0400

    update judgement spec to have unique dates by case

--- spec/models/judgement_spec.rb
+++ spec/models/judgement_spec.rb
@@ -26,18 +26,18 @@ describe Judgement do
 	describe "#self.pct_matched" do
 		it "% of judgements matched to a case" do
 			FactoryGirl.create(:judgement)
-			FactoryGirl.create(:judgement)
-			FactoryGirl.create(:judgement)
+			FactoryGirl.create(:judgement, :case_number => 'case_number_2')
+			FactoryGirl.create(:judgement, :case_number => 'case_number_3')
 			result = Judgement.pct_matched
-			result.should eq(25)
+			result.should eq(75)
 		end
 	end
 	
 	describe "#self.status" do
 		it "return the # of distict judgement status in database" do
-			FactoryGirl.create(:judgement, :status => 'guilty')
-			FactoryGirl.create(:judgement, :status => 'closed')
-			FactoryGirl.create(:judgement, :status => 'dismissed')
+			FactoryGirl.create(:judgement, :status => 'guilty', :case_number => 'case_number_1')
+			FactoryGirl.create(:judgement, :status => 'closed', :case_number => 'case_number_2')
+			FactoryGirl.create(:judgement, :status => 'dismissed', :case_number => 'case_number_3')
 			result = Judgement.status.count
 			result.should eq(3)
 		end
