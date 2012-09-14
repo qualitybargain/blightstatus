4cfd2706987ef7a81fbcc00610c607774bd51695
Eddie A Tejeda <eddie@codeforamerica.org>
2012-09-14 16:54:02 -0700

    rename action views to resemble routes, mode stats helpers into ajaxable json response

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -2,11 +2,71 @@ class StatisticsController < ApplicationController
 
   respond_to :html, :json
 
-  def graphs
+
+  def browse
+		@date = Time.now()
   end
 
-  def maps
-	@date = Time.now()
+  def stats
+
+  	start_date = params[:start_date]
+  	end_date = params[:end_date]
+  	case params[:type]
+
+			when "addresses"
+				@response = {
+											:result => Address.count 
+										}
+
+			when "cases"
+				@response = { 
+											:result => Case.count, 
+										}
+
+			when "inspections"
+				@response = { 
+											:result => Inspection.where(" inspection_date > '#{start_date}' AND inspection_date < '#{end_date}' ").results
+										}
+
+			when "notifications"
+				@response = { 
+											:result => Notification.where(" notification_date > '#{start_date}' AND notification_date < '#{end_date}' ").results
+										}
+
+			when "hearings"
+				@response = { 
+											:result => Hearing.where(" hearing_date > '#{start_date}' AND hearing_date < '#{end_date}' ").status
+										}
+
+			when "resets"
+				@response = { 
+											:result => Reset.where(" reset_date > '#{start_date}' AND reset_date < '#{end_date}' ").status
+										}
+
+			when "judgements"
+				@response = { 
+											:result => Judgement.where(" judgement_date > '#{start_date}' AND judgement_date < '#{end_date}' ").status
+										}
+
+			when "maintenances"
+				@response = { 
+											:result => Maintenance.where(" maintenance_date > '#{start_date}' AND maintenance_date < '#{end_date}' ").status
+										}
+
+			when "foreclosures"
+				@response = { 
+											:result => Foreclosure.where(" foreclosure_date > '#{start_date}' AND foreclosure_date < '#{end_date}' ").status
+										}
+
+			when "demolitions"
+				@response = { 
+											:result => Demolition.where(" demolition_date > '#{start_date}' AND demolition_date < '#{end_date}' ").status
+										}
+		end
+
+    respond_to do |format|
+      format.json { render :json => @response.to_json }
+    end
   end
 
 end
--- app/helpers/statistics_helper.rb
+++ app/helpers/statistics_helper.rb
@@ -1,43 +1,43 @@
 module StatisticsHelper
 
-	def addresses_stats
-		{:total => Address.count}
-	end
+	# def addresses_stats
+	# 	{:total => Address.count}
+	# end
 
-	def cases_stats
-		{:total => Case.count, :matched => Case.matched_count, :unmatched => Case.unmatched_count, :percentageMatche => Case.pct_matched}
-	end
+	# def cases_stats
+	# 	{:total => Case.count, :matched => Case.matched_count, :unmatched => Case.unmatched_count, :percentageMatche => Case.pct_matched}
+	# end
 
-	def inspections_stats
-		{:total => Inspection.count, :matched => Inspection.matched_count, :unmatched => Inspection.unmatched_count, :percentageMatched => Inspection.pct_matched,:types =>Inspection.types,:results => Inspection.results}
-	end
+	# def inspections_stats
+	# 	{:total => Inspection.count, :matched => Inspection.matched_count, :unmatched => Inspection.unmatched_count, :percentageMatched => Inspection.pct_matched,:types =>Inspection.types,:results => Inspection.results}
+	# end
 
-	def notifications_stats
-		{:total => Notification.count, :matched => Notification.matched_count, :unmatched => Notification.unmatched_count, :percentageMatched => Notification.pct_matched, :types => Notification.types}
-	end
+	# def notifications_stats
+	# 	{:total => Notification.count, :matched => Notification.matched_count, :unmatched => Notification.unmatched_count, :percentageMatched => Notification.pct_matched, :types => Notification.types}
+	# end
 
-	def hearings_stats
-		{:total => Hearing.count, :matched => Hearing.matched_count, :unmatched => Hearing.unmatched_count, :percentageMatched => Hearing.pct_matched, :status => Hearing.status}
-	end
+	# def hearings_stats
+	# 	{:total => Hearing.count, :matched => Hearing.matched_count, :unmatched => Hearing.unmatched_count, :percentageMatched => Hearing.pct_matched, :status => Hearing.status}
+	# end
 
-	def resets_stats
-		{:total => Reset.count, :matched => Reset.matched_count, :unmatched => Reset.unmatched_count, :percentageMatched => Reset.pct_matched}
-	end
+	# def resets_stats
+	# 	{:total => Reset.count, :matched => Reset.matched_count, :unmatched => Reset.unmatched_count, :percentageMatched => Reset.pct_matched}
+	# end
 
-	def judgements_stats
-		{:total => Judgement.count, :matched => Judgement.matched_count, :unmatched => Judgement.unmatched_count, :percentageMatched => Judgement.pct_matched, :status => Judgement.status}
-	end
+	# def judgements_stats
+	# 	{:total => Judgement.count, :matched => Judgement.matched_count, :unmatched => Judgement.unmatched_count, :percentageMatched => Judgement.pct_matched, :status => Judgement.status}
+	# end
 
-	def maintenances_stats
-		{:total => Maintenance.count, :matched => Maintenance.matched, :unmatched => Maintenance.unmatched, :percentageMatched => Maintenance.pct_matched, :program_names => Maintenance.program_names, :status => Maintenance.status}
-	end
+	# def maintenances_stats
+	# 	{:total => Maintenance.count, :matched => Maintenance.matched, :unmatched => Maintenance.unmatched, :percentageMatched => Maintenance.pct_matched, :program_names => Maintenance.program_names, :status => Maintenance.status}
+	# end
 
-	def foreclosures_stats
-		{:total => Foreclosure.count, :matched => Foreclosure.matched_count, :unmatched => Foreclosure.unmatched_count, :percentageMatched => Foreclosure.pct_matched, :status => Foreclosure.status}
-	end
+	# def foreclosures_stats
+	# 	{:total => Foreclosure.count, :matched => Foreclosure.matched_count, :unmatched => Foreclosure.unmatched_count, :percentageMatched => Foreclosure.pct_matched, :status => Foreclosure.status}
+	# end
 
-	def demolitions_stats
-		{:total => Demolition.count, :matched => Demolition.matched_count, :unmatched => Demolition.unmatched_count, :percentageMatched => Demolition.pct_matched}
-	end
+	# def demolitions_stats
+	# 	{:total => Demolition.count, :matched => Demolition.matched_count, :unmatched => Demolition.unmatched_count, :percentageMatched => Demolition.pct_matched}
+	# end
 
 end
