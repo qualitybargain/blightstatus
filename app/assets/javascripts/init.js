5acb8ae1247dda5d06491f5fa66de81cd2a5ff43
Serena Wales <serenawr@gmail.com>
2012-09-18 11:35:13 -0700

    Error handling for dropdown form

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -5,6 +5,8 @@ OpenBlight = {
       // application-wide code
       OpenBlight.common.show_disclaimer();
       OpenBlight.common.handle_auto_complete_address();
+      OpenBlight.common.convertDivToSubmit();
+      OpenBlight.common.dropdownLoginForm();
 
       if(!Array.prototype.last) {
           Array.prototype.last = function() {
@@ -12,12 +14,26 @@ OpenBlight = {
           }
       }
 
-
       if(!String.prototype.capitalize) {
         String.prototype.capitalize = function() {
             return this.charAt(0).toUpperCase() + this.slice(1);
         }
       }
+    },
+
+    dropdownLoginForm: function(){
+      $('.dropdown-menu form').submit(function(e){
+        e.preventDefault();
+        var $this = $(this);
+        var req = $.post("/accounts/sign_in", $this.serialize(), function(data){
+          location.reload();
+        });
+        req.error(function(){
+          if(req.status == 401){
+            $this.children('.error').addClass('alert').html("Your email or password (or both) is incorrect.");
+          }
+        });
+      });
 
     },
 
