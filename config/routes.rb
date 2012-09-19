d3271e25eea31980cdabd337cc023b4f6b1c60c6
Serena Wales <serenawr@gmail.com>
2012-09-19 11:54:30 -0700

    Make about page home page

--- config/routes.rb
+++ config/routes.rb
@@ -96,7 +96,7 @@ Openblight::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-  root :to => 'home#index'
+  root :to => 'pages#about'
 
   # See how all your routes lay out with "rake routes"
 
