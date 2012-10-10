a2d137a87ef535a68f4ae17eb0d081149607a2db
Eddie A Tejeda <eddie@codeforamerica.org>
2012-10-09 20:45:05 -0500

    comment out some extra console.logs

--- app/assets/javascripts/controllers/accounts.js
+++ app/assets/javascripts/controllers/accounts.js
@@ -41,12 +41,12 @@ OpenBlight.accounts = {
     if($('#no-subscriptions-found').length){
 
       if($('.subscription').length > 0){
-        console.log('is 0')
+        // console.log('is 0')
         $('#no-subscriptions-found').hide();
         // $('#subscriptions-information').show();
       }
       else{
-        console.log('is greater 0');
+        // console.log('is greater 0');
         $('#no-subscriptions-found').show();
         // $('#subscriptions-information').hide();
       }
@@ -158,12 +158,12 @@ OpenBlight.accounts = {
         onEachFeature: function(feature, layer) {
            $(layer).on('click', function(){
 
-            console.log(data[current_feature_id].id);
+            // console.log(data[current_feature_id].id);
             var select_subcription = "subscription-" + data[current_feature_id].id;
             OpenBlight.common.goToByScroll(select_subcription, 'slow', '150');
             current_feature_id = current_feature_id +1;
 
-            console.log(select_subcription);
+            // console.log(select_subcription);
             $('#' + select_subcription).animate({ backgroundColor: "#FFFFE0" }, 'slow').animate({ backgroundColor: "white" }, 'fast');
           });
         }
